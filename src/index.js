1985e9c56161cac32274a58f50c301acb9efb9fb
z002ytj <jim.beyers@target.com>
2022-05-19 11:52:31 -0500

    first changes

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-require('reveal.js/dist/reveal.css')
-require('reveal.js/dist/theme/black.css')
-let deck = new Reveal({
-    slideNumber: 'c/t',
-    plugins: [ Markdown ]
-})
-deck.initialize();
